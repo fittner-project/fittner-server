5a82a6f12e2581e2c83dcc9f60efecf616018c75
newy12 <newy12@naver.com>
2024-12-16 13:59:53 +0900

    #feat. 멀티모듈 구조화

--- module-api/src/main/java/kr/co/fittnerserver/ModuleApiApplication.java
+++ module-api/src/main/java/kr/co/fittnerserver/ModuleApiApplication.java
@@ -1,14 +1,15 @@
 package kr.co.fittnerserver;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class FittnerServerApplication {
+public class ModuleApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FittnerServerApplication.class, args);
+        SpringApplication.run(ModuleApiApplication.class, args);
     }
 
 }

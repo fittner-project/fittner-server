8c1b78bc248ed376347a21c5b0ef3a56ae634291
namgihyeon <skarlgus0419@naver.com>
2024-12-21 01:42:43 +0900

    #feat. 스웨거 구분 추가

--- module-api/src/main/java/kr/co/fittnerserver/config/SwaggerConfig.java
+++ module-api/src/main/java/kr/co/fittnerserver/config/SwaggerConfig.java
@@ -2,11 +2,39 @@ package kr.co.fittnerserver.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi userGroupApi() {
+        return GroupedOpenApi.builder()
+                .group("1")
+                .displayName("트레이너")
+                .pathsToMatch("/api/v1/user/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi adminGroupApi() {
+        return GroupedOpenApi.builder()
+                .group("2")
+                .displayName("대표")
+                .pathsToMatch("/api/v1/admin/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi commonGroupApi() {
+        return GroupedOpenApi.builder()
+                .group("3")
+                .displayName("공통")
+                .pathsToMatch("/api/v1/common/**")
+                .build();
+    }
 
     @Bean
     public OpenAPI customOpenAPI() {

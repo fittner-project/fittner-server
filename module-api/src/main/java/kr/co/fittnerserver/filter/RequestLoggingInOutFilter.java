37551a4e1e000ff14fa42691336990614c399112
namgihyeon <skarlgus0419@naver.com>
2024-12-21 01:01:18 +0900

    #feat. swagger 로그 제외

--- module-api/src/main/java/kr/co/fittnerserver/filter/RequestLoggingInOutFilter.java
+++ module-api/src/main/java/kr/co/fittnerserver/filter/RequestLoggingInOutFilter.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 @Component
 @Slf4j
@@ -24,6 +25,7 @@ public class RequestLoggingInOutFilter extends OncePerRequestFilter {
     private String RESPONSE_INFO_PRI_FIX = "-----------------------[RESPONSE-INFO]-----------------------";
     private String REQUEST_PRI_FIX = "--->";
     private String RESPONSE_PRI_FIX = "<---";
+    private String[] NOT_ALLOW_LOG = {"/swagger-ui","/v3/api-docs"};
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -31,45 +33,55 @@ public class RequestLoggingInOutFilter extends OncePerRequestFilter {
         ContentCachingResponseWrapper wrapperResponse = wrapperRespnse(response);
         ContentCachingRequestWrapper wrapperRequest = wrapperRequest(request);
 
+        //log출력 여부
+        boolean logChkeck = Arrays.stream(NOT_ALLOW_LOG)
+                .anyMatch(item -> wrapperRequest.getRequestURI().contains(item));
+
         //request log
-        beforeRequest(wrapperRequest);
+        beforeRequest(wrapperRequest, !logChkeck);
         //return
         filterChain.doFilter(wrapperRequest,wrapperResponse);
         //response log
-        afterRequest(wrapperResponse);
+        afterRequest(wrapperResponse, !logChkeck);
 
         //client response copy
         wrapperResponse.copyBodyToResponse();
     }
 
-    protected void beforeRequest(ContentCachingRequestWrapper wrapperRequest) throws IOException{
-        wrapperRequest.getParameterNames();
-        byte[] content = wrapperRequest.getContentAsByteArray();
-        String contentStr = URLDecoder.decode(new String(content, wrapperRequest.getCharacterEncoding()),String.valueOf(StandardCharsets.UTF_8));
+    protected void beforeRequest(ContentCachingRequestWrapper wrapperRequest, boolean logChkeck) throws IOException{
 
-        log.info(REQUEST_INFO_PRI_FIX);
-        log.info("{} client ip ::: {}", RESPONSE_PRI_FIX, Util.getClientIp(wrapperRequest));
-        log.info("{} url ::: {}", REQUEST_PRI_FIX, wrapperRequest.getRequestURI());
-        log.info("{} method ::: {}", REQUEST_PRI_FIX, wrapperRequest.getMethod());
-        log.info("{} content-type ::: {}", REQUEST_PRI_FIX, wrapperRequest.getContentType());
-        log.info("{} content ::: {}", REQUEST_PRI_FIX, contentStr);
+        if(logChkeck){
+            wrapperRequest.getParameterNames();
+            byte[] content = wrapperRequest.getContentAsByteArray();
+            String contentStr = URLDecoder.decode(new String(content, wrapperRequest.getCharacterEncoding()),String.valueOf(StandardCharsets.UTF_8));
+
+            log.info(REQUEST_INFO_PRI_FIX);
+            log.info("{} client ip ::: {}", RESPONSE_PRI_FIX, Util.getClientIp(wrapperRequest));
+            log.info("{} url ::: {}", REQUEST_PRI_FIX, wrapperRequest.getRequestURI());
+            log.info("{} method ::: {}", REQUEST_PRI_FIX, wrapperRequest.getMethod());
+            log.info("{} content-type ::: {}", REQUEST_PRI_FIX, wrapperRequest.getContentType());
+            log.info("{} content ::: {}", REQUEST_PRI_FIX, contentStr);
+        }
     }
 
-    protected void afterRequest(ContentCachingResponseWrapper wrapperResponse) throws IOException{
-        byte[] content = wrapperResponse.getContentAsByteArray();
-        String contentStr = new String(content, wrapperResponse.getCharacterEncoding());
-        if(wrapperResponse.getContentType() != null){
-            //log 출력제외
-            if(wrapperResponse.getContentType().startsWith("text/html")){
-                contentStr = "html result";
+    protected void afterRequest(ContentCachingResponseWrapper wrapperResponse, boolean logChkeck) throws IOException{
+
+        if(logChkeck){
+            byte[] content = wrapperResponse.getContentAsByteArray();
+            String contentStr = new String(content, wrapperResponse.getCharacterEncoding());
+            if(wrapperResponse.getContentType() != null){
+                //log 출력제외
+                if(wrapperResponse.getContentType().startsWith("text/html")){
+                    contentStr = "html result";
+                }
+                if(wrapperResponse.getContentType().startsWith("image/")){
+                    contentStr = "image result";
+                }
             }
-            if(wrapperResponse.getContentType().startsWith("image/")){
-                contentStr = "image result";
-            }
+            log.info(RESPONSE_INFO_PRI_FIX);
+            log.info("{} http status ::: {}",RESPONSE_PRI_FIX, wrapperResponse.getStatus());
+            log.info("{} result ::: {}", RESPONSE_PRI_FIX, contentStr);
         }
-        log.info(RESPONSE_INFO_PRI_FIX);
-        log.info("{} http status ::: {}",RESPONSE_PRI_FIX, wrapperResponse.getStatus());
-        log.info("{} result ::: {}", RESPONSE_PRI_FIX, contentStr);
     }
 
     private ContentCachingRequestWrapper wrapperRequest(HttpServletRequest request){

72ee127c5a45d031391e251e79d62761cc4b7cc3
youngjae <newy12@naver.com>
2024-12-23 22:08:14 +0900

    #feat. @Schema 적용안된부분 추가

--- module-database/src/main/java/kr/co/fittnerserver/dto/user/TokenResDto.java
+++ module-database/src/main/java/kr/co/fittnerserver/dto/user/TokenResDto.java
@@ -1,5 +1,6 @@
 package kr.co.fittnerserver.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenResDto {
+    @Schema(description = "액세스 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0cmFpbmVyQG5hdmVyLmNvbSIsImV4cCI6MTYzNzQwNjQwNn0.7")
     private String accessToken;
+    @Schema(description = "리프레시 토큰",example = "alskdnksla")
     private String refreshTokenId;
 }

6bd02b10f721ce2a52748e264f9a6f3ae85dbd60
youngjae <newy12@naver.com>
2024-12-23 22:04:48 +0900

    #feat. @Schema 쓸수 있도록 수정

--- module-database/src/main/java/kr/co/fittnerserver/dto/user/LoginRequestDto.java
+++ module-database/src/main/java/kr/co/fittnerserver/dto/user/LoginRequestDto.java
@@ -1,8 +1,10 @@
 package kr.co.fittnerserver.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class LoginRequestDto {
+    @Schema(description = "이메일", example = "newy12@naver.com")
     private String trainerEmail;
 }

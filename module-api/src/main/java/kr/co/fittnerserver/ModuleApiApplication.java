5514cd9a01b8399799d7630ed67b32aca2f0a6ed
youngjae <newy12@naver.com>
2025-01-01 11:47:08 +0900

    #feat. module-api만 배포했을때 스크립트 반응 확인

--- module-api/src/main/java/kr/co/fittnerserver/ModuleApiApplication.java
+++ module-api/src/main/java/kr/co/fittnerserver/ModuleApiApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ModuleApiApplication {
 
     public static void main(String[] args) throws Exception {
-        //배포 테스트11
+        //API 배포
         SpringApplication.run(ModuleApiApplication.class, args);
     }
 

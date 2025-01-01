a09b0de6702cb6334e9fc136e4717b6e66fcfd12
youngjae <newy12@naver.com>
2025-01-01 11:49:41 +0900

    #feat. module-database만 배포했을때 스크립트 반응 확인

--- module-database/src/main/java/kr/co/fittnerserver/repository/user/TrainerRepository.java
+++ module-database/src/main/java/kr/co/fittnerserver/repository/user/TrainerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TrainerRepository extends JpaRepository<Trainer,String> {
+    //배포
     Optional<Trainer> findByTrainerEmail(String trainerEmail);
 
     boolean existsByTrainerEmail(String encrypt);

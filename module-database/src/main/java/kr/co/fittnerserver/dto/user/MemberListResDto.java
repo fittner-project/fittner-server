047adedf30b73b031edf79182008b5c0cf967f13
youngjae <newy12@naver.com>
2025-01-05 00:37:09 +0900

    #feat. 트레이너의 회원 조회 api 추가

---
+++ module-database/src/main/java/kr/co/fittnerserver/dto/user/MemberListResDto.java
@@ -0,0 +1,24 @@
+package kr.co.fittnerserver.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberListResDto {
+    @Schema(description = "회원 ID", example = "1")
+    private String memberId;
+    @Schema(description = "회원 이름", example = "홍길동")
+    private String memberName;
+    @Schema(description = "회원 전화번호", example = "010-1234-5678")
+    private String memberPhone;
+    @Schema(description = "회원 나이", example = "30")
+    private String memberAge;
+    @Schema(description = "회원 총 수", example = "10")
+    private long memberTotalCount;
+}
--- module-database/src/main/java/kr/co/fittnerserver/repository/user/MemberRepository.java
+++ module-database/src/main/java/kr/co/fittnerserver/repository/user/MemberRepository.java
@@ -1,8 +1,12 @@
 package kr.co.fittnerserver.repository.user;
 
 import kr.co.fittnerserver.entity.user.Member;
+import kr.co.fittnerserver.entity.user.Trainer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface MemberRepository extends JpaRepository<Member,String> {
+public interface MemberRepository extends JpaRepository<Member, String> {
+    Page<Member> findAllByTrainer(Trainer trainer, Pageable pageable);
 }

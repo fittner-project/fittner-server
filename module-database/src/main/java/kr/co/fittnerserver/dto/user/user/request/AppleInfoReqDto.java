a00903afbf484bca625e77fa38e53b45db81fc3b
youngjae <newy12@naver.com>
2025-01-14 21:45:20 +0900

    #feat. 스키마 추가

--- module-database/src/main/java/kr/co/fittnerserver/dto/user/user/request/AppleInfoReqDto.java
+++ module-database/src/main/java/kr/co/fittnerserver/dto/user/user/request/AppleInfoReqDto.java
@@ -1,8 +1,10 @@
 package kr.co.fittnerserver.dto.user.user.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class AppleInfoReqDto {
+    @Schema(description = "애플 로그인 시 필요한 인증코드", example = "201205211618573161SP")
     private String code;
 }

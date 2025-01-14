3e553d381fbf0f44d9a55177c7dcce5721075eb2
youngjae <newy12@naver.com>
2025-01-14 21:50:03 +0900

    #feat. 스키마 추가

--- module-database/src/main/java/kr/co/fittnerserver/dto/user/user/response/AppleInfoResDto.java
+++ module-database/src/main/java/kr/co/fittnerserver/dto/user/user/response/AppleInfoResDto.java
@@ -1,5 +1,6 @@
 package kr.co.fittnerserver.dto.user.user.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AppleInfoResDto {
+    @Schema(description = "애플 로그인 시 필요한 유저이메일", example = "newy12@naver.com")
     private String userEmail;
 }

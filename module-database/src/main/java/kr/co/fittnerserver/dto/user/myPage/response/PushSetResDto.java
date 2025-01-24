0c1f16bf6a1c9c1581f221a939b18a9a0f25c403
namgihyeon <skarlgus0419@naver.com>
2025-01-24 22:07:32 +0900

    #feat. 알림설정 api 수정

--- module-database/src/main/java/kr/co/fittnerserver/dto/user/myPage/response/PushSetResDto.java
+++ module-database/src/main/java/kr/co/fittnerserver/dto/user/myPage/response/PushSetResDto.java
@@ -1,10 +1,14 @@
 package kr.co.fittnerserver.dto.user.myPage.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class PushSetResDto {
 
+    @Schema(description = "푸시종류", example = "ADVERTISE | RESERVATION")
     private String pushKind;
+
+    @Schema(description = "푸시여부", example = "Y")
     private String pushSetYn;
 }

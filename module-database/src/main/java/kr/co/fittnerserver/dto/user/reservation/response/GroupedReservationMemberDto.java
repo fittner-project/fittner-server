4c12a827600fdb9b7013543a7cb51ecb7e19b8db
youngjae <newy12@naver.com>
2025-01-28 12:34:15 +0900

    #feat. res 보충

--- module-database/src/main/java/kr/co/fittnerserver/dto/user/reservation/response/GroupedReservationMemberDto.java
+++ module-database/src/main/java/kr/co/fittnerserver/dto/user/reservation/response/GroupedReservationMemberDto.java
@@ -1,5 +1,6 @@
 package kr.co.fittnerserver.dto.user.reservation.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +9,8 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class GroupedReservationMemberDto {
+    @Schema(description = "날짜 끝두자리 값", example = "02")
     private String lastTwoDigits;
+    @Schema(description = "예액 정보들", example = " ")
     private List<ReservationMemberResDto> reservations;
 }

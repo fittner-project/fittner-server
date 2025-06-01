30ba4ff9c249e4a68634bfbf74a988b32f7e34e0
youngjae <newy12@naver.com>
2025-06-01 19:03:10 +0900

    #feat. 수정

--- module-database/src/main/java/kr/co/fittnerserver/repository/user/ReservationRepository.java
+++ module-database/src/main/java/kr/co/fittnerserver/repository/user/ReservationRepository.java
@@ -16,35 +16,40 @@ public interface ReservationRepository extends JpaRepository<Reservation,String>
 
 
     @Query(value = """
-              SELECT new kr.co.fittnerserver.dto.user.reservation.response.ReservationMemberResDto(
-                    RIGHT(r.reservationStartDate, 2),
-                    m.memberName,
-                    r.reservationStartDate,
-                    r.reservationEndDate,
-                    r.reservationStartTime,
-                    r.reservationEndTime,
-                    CAST(r.reservationColor AS string),
-                    r.reservationMemo,
-                    tk.ticketUseCnt + 1,
-                    tp.trainerProductCount
-              )
-              FROM Reservation r
-              LEFT JOIN Trainer t
-              ON r.trainer.trainerId = t.trainerId
-              LEFT JOIN Ticket tk
-              ON r.ticket.ticketId = tk.ticketId
-              LEFT JOIN Member m
-              ON r.member.memberId = m.memberId
-              LEFT JOIN TrainerProduct tp
-              on r.trainer.trainerId = tp.trainer.trainerId
-              WHERE m.memberDeleteYn = 'N'
-              AND r.reservationDeleteYn = 'N'
-              AND t.trainerStatus != 'DROP'
-              AND t.trainerId = :trainerId
-              AND r.reservationStartDate >= :reservationStartDate
-              AND r.reservationEndDate <= :reservationEndDate
-              ORDER BY RIGHT(r.reservationStartDate, 2) ASC,r.reservationStartDate ASC, r.reservationStartTime ASC
+          SELECT new kr.co.fittnerserver.dto.user.reservation.response.ReservationMemberResDto(
+                RIGHT(r.reservationStartDate, 2),
+                m.memberName,
+                r.reservationStartDate,
+                r.reservationEndDate,
+                r.reservationStartTime,
+                r.reservationEndTime,
+                CAST(r.reservationColor AS string),
+                r.reservationMemo,
+                tk.ticketUseCnt + 1,
+                tp.trainerProductCount
+          )
+          FROM Reservation r
+          LEFT JOIN Trainer t
+          ON r.trainer.trainerId = t.trainerId
+          LEFT JOIN Ticket tk
+          ON r.ticket.ticketId = tk.ticketId
+          LEFT JOIN Member m
+          ON r.member.memberId = m.memberId
+          LEFT JOIN TrainerProduct tp
+          ON tp.trainerProductId = (
+              SELECT MIN(tp2.trainerProductId)
+              FROM TrainerProduct tp2
+              WHERE tp2.trainer.trainerId = r.trainer.trainerId
+          )
+          WHERE m.memberDeleteYn = 'N'
+          AND r.reservationDeleteYn = 'N'
+          AND t.trainerStatus != 'DROP'
+          AND t.trainerId = :trainerId
+          AND r.reservationStartDate >= :reservationStartDate
+          AND r.reservationEndDate <= :reservationEndDate
+          ORDER BY RIGHT(r.reservationStartDate, 2) ASC, r.reservationStartDate ASC, r.reservationStartTime ASC
 """)
+
     List<ReservationMemberResDto> getReservationMemberDataList(@Param(value = "reservationStartDate") String reservationStartDate,
                                                                 @Param(value = "reservationEndDate") String reservationEndDate,
                                                                @Param(value = "trainerId") String trainerId);

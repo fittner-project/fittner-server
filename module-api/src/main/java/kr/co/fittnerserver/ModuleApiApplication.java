91d8d5032e60233abf3d471ce4d64696f0ca6b4a
youngjae <newy12@naver.com>
2025-09-08 20:40:02 +0900

    #feat. 배포테스트

--- module-api/src/main/java/kr/co/fittnerserver/ModuleApiApplication.java
+++ module-api/src/main/java/kr/co/fittnerserver/ModuleApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ModuleApiApplication {
     public static void main(String[] args) throws Exception {
+        //배포 테스트
         SpringApplication.run(ModuleApiApplication.class, args);
     }
 

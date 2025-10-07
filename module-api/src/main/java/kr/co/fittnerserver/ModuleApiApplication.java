6e8c0ddb7742081eccd9cb9186ff592b6e9c33d5
youngjae <newy12@naver.com>
2025-10-07 21:48:19 +0900

    #feat. 배포커밋

--- module-api/src/main/java/kr/co/fittnerserver/ModuleApiApplication.java
+++ module-api/src/main/java/kr/co/fittnerserver/ModuleApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModuleApiApplication {
+    //배포ㅡㄹ 위한 커밋
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ModuleApiApplication.class, args);
     }
--- module-batch/src/main/java/kr/co/fittnerserver/ModuleBatchApplication.java
+++ module-batch/src/main/java/kr/co/fittnerserver/ModuleBatchApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class ModuleBatchApplication {
+    //배포를 위한 커밋
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ModuleBatchApplication.class, args);
     }

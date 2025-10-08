98f4d52d1384dd9f56f8c600f5b858a7965bf638
namgihyeon <skarlgus0419@naver.com>
2025-10-08 22:20:28 +0900

    [feat] db시간 set 임시 소스

--- module-database/src/main/java/kr/co/fittnerserver/entity/common/BaseTimeEntity.java
+++ module-database/src/main/java/kr/co/fittnerserver/entity/common/BaseTimeEntity.java
@@ -13,6 +13,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @Getter
@@ -36,12 +37,14 @@ public class BaseTimeEntity {
 
     @PrePersist
     public void onPrePersist(){
-        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        //TODO JVM 설정되어있는데 db시간이 한국시간으로 설정안됨(임시방편으로 Asia/Seoul set)
+        this.createdDate = LocalDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         this.modifiedDate = this.createdDate;
     }
 
     @PreUpdate
     public void onPreUpdate(){
-        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        //TODO JVM 설정되어있는데 db시간이 한국시간으로 설정안됨(임시방편으로 Asia/Seoul set)
+        this.modifiedDate = LocalDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
